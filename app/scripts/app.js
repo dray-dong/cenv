ed98f90776e6bf7a39719a323fc9a90782a449c1
lei.dong <lei.dong@powerleader.com.cn>
2016-07-07 15:08:25 +0800

    add some file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,21 +15,37 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.router',
+    'ui.bootstrap'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+  .config(function($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise('/');
+            $stateProvider
+            .state('main', {
+                url:'/',
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+            })
+            .state('about', {
+                url:'/about',
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl',
+            })
   });
+  //   .config(function ($routeProvider) {
+  //   $routeProvider
+  //     .when('/', {
+  //       templateUrl: 'views/main.html',
+  //       controller: 'MainCtrl',
+  //       controllerAs: 'main'
+  //     })
+  //     .when('/about', {
+  //       templateUrl: 'views/about.html',
+  //       controller: 'AboutCtrl',
+  //       controllerAs: 'about'
+  //     })
+  //     .otherwise({
+  //       redirectTo: '/'
+  //     });
+  // });
